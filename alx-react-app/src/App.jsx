eee76e2baa67299edb2988d04389e8789c316558
joey5573 <Siff.fox@gmail.com>
2025-10-12 03:14:40 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <UserProfile name="John Doe" age="28" location="Nairobi" />
       <Footer />
     </div>
   );
